0fe962f91f0a7da44dbc2dcfbde76708d488cbd1
Eduardo Patterlini <130774571+epatterlini@users.noreply.github.com>
2023-11-10 11:58:11 -0300

    add Pedido

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
 var valor;
 var suma = 0;
 let precios = 0;
+let descuento;
+var esCliente;
+var selected;
 const valorCliente = 0.30;
 
 function eje1()
 {
     valor = document.getElementsByName("nombre")[0].value;
-    window.alert("Gracias por tu comentario "+valor+", lo valoramos mucho!.");
+    window.alert("Gracias por tu comentario "+ valor +", lo valoramos mucho!.");
     window.location.reload();
 }
 
 function Pedido()
 {
     window.open("Pedido.html");
+    setInterval("location.reload()",2000);
 }
 
 function load()
 {
-    window.alert(document.cookie);
-    document.getElementById("precioTotal").value = "VALOR TOTAL $"+ document.cookie;;
+    document.getElementById("precioTotal").innerHTML = "VALOR TOTAL $"+ document.cookie;
+    descuento = document.cookie - document.cookie * valorCliente;
+    document.getElementById("valorDescuento").innerHTML = "TOTAL A PAGAR SIENDO CLIENTE: $"+ descuento;
 }
 
-function seleccion()
+function onClose()
+{
+    precios = 0;
+    suma = 0;
+    document.cookie = encodeURIComponent(precios);
+    window.close();
+}
+
+function seleccion(precios)
 {
     if(suma != 0)
     {
-        precios =  suma + parseInt(document.getElementById("precio").innerHTML);
+        precios =  suma + precios
         suma = precios;
-        window.alert("El precio es "+precios);
-        document.cookie = encodeURIComponent(precios); 
+        window.alert("El precio es $"+ precios);
+        document.cookie = encodeURIComponent(precios);
     }else
     {
-        precios =  parseInt(document.getElementById("precio").innerHTML);
         suma = precios;
-        elPrecio = precios;
-        window.alert("El precio es "+precios);
+        window.alert("El precio es $"+ precios);
         document.cookie = encodeURIComponent(precios);
     }
+}
+
+function cliente()
+{
+    esCliente = document.getElementById("esCliente");
+    selected = esCliente.options[esCliente.selectedIndex].text;
+}
+
+function compra()
+{
+    let nombreComprador = document.getElementsByName("nombre")[0].value;
+    let apellidoComprador = document.getElementsByName("apellido")[0].value;
+    let domicilio = document.getElementsByName("domicilio")[0].value;
+    if(nombreComprador != "" && apellidoComprador != "" && domicilio != "")
+    {
+        if(selected == "Si")
+        {
+            window.alert("Gracias "+ nombreComprador + " " + apellidoComprador + " por su compra!\nEl pedido costará $" +
+        descuento + " por ser nuestro cliente!.\nSe enviará a " + domicilio);
+        }
+        else
+        {
+            window.alert("Gracias "+ nombreComprador + " " + apellidoComprador + " por su compra!\nEl pedido costará $" +
+            document.cookie + "\nSe enviará a " + domicilio);
+        }
+    }
+    else
+    {
+        window.alert("Debe llenar los campos de Nombre, Apellido y Domicilio.");
+    }
+    
 }
 
 
